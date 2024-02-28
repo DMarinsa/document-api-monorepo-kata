3f800a1dc21329ca9e87267818bcc348ae97d76a
David Marin Sanchez <dmarin@theknotww.com>
2024-02-28 22:27:22 +0100

    add pgConnector and tests

--- shared/pg-connector/src/lib/pg-connector.spec.ts
+++ shared/pg-connector/src/lib/pg-connector.spec.ts
@@ -1,7 +1,110 @@
-import { pgConnector } from './pg-connector';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { PGConnector, PostgresPoolOptions } from './pg-connector';
+import { Pool, QueryConfig, QueryResult } from 'pg';
+
+jest.mock('pg', () => {
+  const mPoolClient = {
+    query: jest.fn(() => ({
+      rows: [{ id: 1, name: 'Test' }],
+      rowCount: 1,
+      command: 'SELECT',
+      oid: 0,
+      fields: []
+    })),
+    release: jest.fn()
+  };
+  const mPool = {
+    connect: jest.fn().mockResolvedValue(mPoolClient),
+    end: jest.fn()
+  };
+  return {
+    Pool: jest.fn(() => mPool)
+  };
+});
+
+function createMockPool(): any {
+  return new (Pool as jest.MockedClass<typeof Pool>)();
+}
 
 describe('pgConnector', () => {
-  it('should work', () => {
-    expect(pgConnector()).toEqual('pg-connector');
+
+  let pgConnector: PGConnector;
+  let mockPool: jest.Mocked<Pool>;
+
+  beforeEach(() => {
+    const postgresOptions: PostgresPoolOptions = {
+      user: 'test_user',
+      password: 'test_password',
+      host: 'test_host',
+      database: 'test_db',
+      port: 5432
+    };
+    mockPool = createMockPool();
+    pgConnector = new PGConnector(postgresOptions);
+    (pgConnector as any).pool = mockPool;
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('instantiates', () => {
+    const connector = new PGConnector({ connectionString: 'test' });
+    expect(connector).toBeInstanceOf(PGConnector);
+  })
+
+  it('exposes a query method', () => {
+    const connector = new PGConnector({ connectionString: 'test' });
+    
+    expect(connector.query).toBeInstanceOf(Function);
+  });
+
+  it('exposes a close method', () => {
+    const connector = new PGConnector({ connectionString: 'test' });
+    
+    expect(connector.close).toBeInstanceOf(Function);
+  });
+
+  describe('query', () => {
+    it('calls pool.connect and pool.query with the given queryConfig', async () => {
+      const queryConfig: QueryConfig = {
+        text: 'SELECT * FROM test_table'
+      };
+      const expectedResult: QueryResult<any> = {
+        rows: [{ id: 1, name: 'Test' }],
+        rowCount: 1,
+        command: 'SELECT',
+        oid: 0,
+        fields: []
+      };
+
+      const result = await pgConnector.query(queryConfig);
+
+      expect(result).toEqual(expectedResult);
+
+      expect(mockPool.connect).toHaveBeenCalledTimes(1);
+      expect((await mockPool.connect()).query).toHaveBeenCalledWith(queryConfig);
+    });
+
+    it('should release the client after executing the query', async () => {
+      const queryConfig: QueryConfig = {
+        text: 'SELECT * FROM test_table'
+      };
+
+      await pgConnector.query(queryConfig);
+
+      expect(mockPool.connect).toHaveBeenCalledTimes(1);
+      expect((await mockPool.connect()).release).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('close', () => {
+    it('closes the connection', async () => {
+      const connector = new PGConnector({ connectionString: 'test' });
+      
+      await connector.close();
+
+      expect(mockPool.end).toHaveBeenCalledTimes(1);
+    });
   });
 });
--- shared/pg-connector/src/lib/pg-connector.ts
+++ shared/pg-connector/src/lib/pg-connector.ts
@@ -1,3 +1,25 @@
-export function pgConnector(): string {
-  return 'pg-connector';
+// postgresPool.ts
+import { Pool, PoolConfig, QueryConfig, QueryResult, QueryResultRow } from 'pg';
+
+export type PostgresPoolOptions = PoolConfig
+
+export class PGConnector {
+  private pool: Pool;
+
+  constructor(options: PostgresPoolOptions) {
+    this.pool = new Pool(options);
+  }
+
+  async query(queryConfig: QueryConfig): Promise<QueryResult<QueryResultRow>> {
+    const client = await this.pool.connect();
+    try {
+      return client.query(queryConfig);
+    } finally {
+      client.release();
+    }
+  }
+
+  async close(): Promise<void> {
+    await this.pool.end();
+  }
 }
